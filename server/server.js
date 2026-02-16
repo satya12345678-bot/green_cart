90e19a0f3fc66b72c53b1903deca61e70898dde8
satyabrata Biswal <satyabratta11@gmail.com>
2026-02-16 22:06:40 +0530

    Add allowed origin for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ await connectCloudinary();
 
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://green-cart-three-xi.vercel.app']
 
 app.post('/stripe',express.raw({type :'application/json'}),stripeWebhooks)
 
